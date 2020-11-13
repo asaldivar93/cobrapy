0ab2c5479d688d3407cb50e226c6cdffdbf0b53f
asaldivar93 <asaldivarg93@gmail.com>
2020-11-13 14:57:55 -0600

    x

--- src/cobra/flux_analysis/sparse.py
+++ src/cobra/flux_analysis/sparse.py
@@ -4,21 +4,31 @@ from cobra.flux_analysis.helpers import normalize_cutoff
 from optlang.symbolics import Zero
 
 
-def build_sparse_model(model, keep_boundaries=False):
-    """Adds auxiliary y and associated constraint for every metabolite
+def find_leaks(model, keep_boundaries=False):
+    """Following [ref] finds all the posible leaks of a model
+    by maximizing the following program:
+
+    max_y,v ||y||_0
+    s.t. S*v - y = 0
+         0 <= y for met in metabolites
+         lb <= v <= ub
+
+    Since ||y||_0 is an NP-Hard problem, this is re casted
+    as a LP using L1-norm as an approximation
     Parameters
     ----------
-    model: cobra.core.Model
-        The cobra Model to add vars and constraints to
-    keep_boundaries: boolean
-        A boolean indicating whether or not to keep boundary reactions
-    Returns
-    -------
     sparse_model: cobra.core.Model
-        A cobra Model with auxiliary vars and constrains
-    met_vars : list
+        The cobra model (with aux vars and constrains) to find leaks from
+    met_vars: list
         A list of (optlang.interface.Variable) auxiliary vars for
         every metabolite
+    Returns
+    -------
+    leaks: list
+        The list of meatabolites considered leaks.
+    References
+    -------
+    [ref]
     """
     sparse_model = model.copy()
     prob = sparse_model.problem
@@ -62,55 +72,23 @@ def build_sparse_model(model, keep_boundaries=False):
 
         met_vars.extend([met_var])
 
-    return sparse_model, met_vars
-
-
-def find_leaks(sparse_model, met_vars):
-    """Following [ref] finds all the posible leaks of a model
-    by maximizing the following program:
-
-    max_y,v ||y||_0
-    s.t. S*v - y = 0
-         0 <= y for met in metabolites
-         lb <= v <= ub
-
-    Since ||y||_0 is an NP-Hard problem, this is re casted
-    as a LP using L1-norm as an approximation
-    Parameters
-    ----------
-    sparse_model: cobra.core.Model
-        The cobra model (with aux vars and constrains) to find leaks from
-    met_vars: list
-        A list of (optlang.interface.Variable) auxiliary vars for
-        every metabolite
-    Returns
-    -------
-    leaks: list
-        The list of meatabolites considered leaks.
-    References
-    -------
-    [ref]
-    """
-    w_model = sparse_model.copy()
-    prob = w_model.problem
-    zero_cutoff = normalize_cutoff(w_model, None)
-
+    zero_cutoff = normalize_cutoff(sparse_model, None)
     # Set objective to max(sum(met_vars))
-    w_model.objective = prob.Objective(Zero, direction='max')
-    w_model.objective.set_linear_coefficients({o: 1 for o in met_vars})
+    sparse_model.objective = prob.Objective(Zero, direction='max')
+    sparse_model.objective.set_linear_coefficients({o: 1 for o in met_vars})
 
-    w_model.optimize()
+    sparse_model.optimize()
     leaks = []
     # if y_i > 0 met_i is a leak in the model
-    for var in w_model.variables:
+    for var in sparse_model.variables:
         if 'aux' in var.name:
-            if var.primal > zero_cutoff:
+            if abs(var.primal) > zero_cutoff:
                 leaks.append([var.name.replace('aux_', ''), var.primal])
 
     return leaks
 
 
-def find_leak_mode(sparse_model, leaks=[], cutoff_mult=1):
+def find_leak_mode(model, leaks=[], cutoff_mult=1, keep_boundaries=False):
     """ The minimal set of reactions needed for production
     of leak metabolites (leak_mode) cand be found by min of
     the following program:
@@ -148,14 +126,53 @@ def find_leak_mode(sparse_model, leaks=[], cutoff_mult=1):
     [ref]
     """
 
-    w_model = sparse_model.copy()
-    prob = w_model.problem
-    zero_cutoff = normalize_cutoff(w_model, None)
+    sparse_model = model.copy()
+    prob = sparse_model.problem
+    zero_cutoff = normalize_cutoff(sparse_model, None)
     objective = Zero
     rxn_vars_and_cons = []
 
+    # keep only internal reactions
+    if not keep_boundaries:
+        for rxn in sparse_model.reactions:
+            if rxn.boundary:
+                sparse_model.remove_reactions(rxn)
+
+    # empty all constraints
+    for const in sparse_model.constraints:
+        sparse_model.remove_cons_vars(const)
+
+    met_vars = []
+    for met in sparse_model.metabolites:
+        # Create auxiliary variable y
+        met_var = prob.Variable("aux_{}".format(met.id),
+                                lb=0
+                                )
+        # Create constraint
+        const = prob.Constraint(Zero,
+                                name=met.id,
+                                lb=0,
+                                ub=0
+                                )
+        sparse_model.add_cons_vars([met_var, const])
+
+        rxn_coeffs = []
+        # Get stoichiometrich coefficients
+        # for all reactions of met
+        for rxn in met.reactions:
+            coeff = rxn.metabolites[met]
+            rxn_coeffs.append([rxn.forward_variable, coeff])
+            rxn_coeffs.append([rxn.reverse_variable, -coeff])
+        # Add auxiliary var to constraint
+        rxn_coeffs.append([met_var, -1])
+        # Add constraint to model
+        rxn_coeffs = dict(rxn_coeffs)
+        sparse_model.constraints.get(met.id).set_linear_coefficients(rxn_coeffs)
+
+        met_vars.extend([met_var])
+
     # Add z variables and associated constraints
-    for rxn in w_model.reactions:
+    for rxn in sparse_model.reactions:
         rxn_var = prob.Variable("rxn_{}".format(rxn.id))
 
         const = prob.Constraint(rxn.forward_variable + rxn.reverse_variable - rxn_var,
@@ -166,23 +183,23 @@ def find_leak_mode(sparse_model, leaks=[], cutoff_mult=1):
         rxn_vars_and_cons.extend([rxn_var, const])
         objective += rxn_var**2
 
-    w_model.add_cons_vars(rxn_vars_and_cons)
+    sparse_model.add_cons_vars(rxn_vars_and_cons)
 
     # Add objective. Since solvers only take linear or quadratic objectives
     # the objective function is left as sum_r(z_r**2)
-    w_model.objective = prob.Objective(objective, direction='min')
+    sparse_model.objective = prob.Objective(objective, direction='min')
 
     # find active reactions for every leak individually
     leak_modes = {}
     for leak, flux in leaks:
         rxns_in_mode = []
 
-        met_var = w_model.variables.get("aux_{}".format(leak))
+        met_var = sparse_model.variables.get("aux_{}".format(leak))
         met_var.lb = flux
-        sol = w_model.optimize()
+        sol = sparse_model.optimize()
         met_var.lb = 0
 
-        rxns_in_mode = [[rxn.id, sol.fluxes[rxn.id]] for rxn in w_model.reactions
+        rxns_in_mode = [[rxn.id, sol.fluxes[rxn.id]] for rxn in sparse_model.reactions
                         if abs(sol.fluxes[rxn.id]) >= cutoff_mult * zero_cutoff]
         leak_modes[leak] = rxns_in_mode
         print(leak, sol.status, len(rxns_in_mode))
